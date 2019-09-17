5f13e518da2aea206c422ed18119fe50563b112e
diebuntu08 <diego.garromolina@ucr.ac.cr>
2019-09-17 13:25:55 -0600

    Se abre el archivo log.txt desde main.py

--- main.py
+++ main.py
@@ -17,8 +17,13 @@ cometidos por el personal de cada estación y que se aprenda de ellos.
 
 from datetime import datetime, timedelta
 
+log = open("log.txt", "w")
+
 hoy = datetime.utcnow()
 
 
-url_es = 'http://ogimet.com/display_synops2.php?lugar=78764+78762+78774+78767&tipo=ALL&ord=DIR&nil=SI&fmt=txt&ano=%s&mes=%s&day=%s&hora=%s&anof=%s&mesf=%s&dayf=%s&horaf=%s&enviar=Ver'%(annio_6h, mes_6h, dia_6h, hora_6h, annio, mes, dia, hora)
-url_en = 'https://www.ogimet.com/display_synops2.php?lang=en&lugar=78764+78762+78774+78767&tipo=ALL&ord=DIR&nil=SI&fmt=txt&ano=%s&mes=%s&day=%s&hora=%s&anof=%s&mesf=%s&dayf=%s&horaf=%s&send=send'%(annio_6h, mes_6h, dia_6h, hora_6h, annio, mes, dia, hora)
+url_es = 'http://ogimet.com/display_synops2.php?lugar=78764+78762+78774+78767&tipo=ALL&ord=DIR&nil=SI&fmt=txt&ano=%s&mes=%s&day=%s&hora=%s&anof=%s&mesf=%s&dayf=%s&horaf=%s&enviar=Ver'
+url_en = 'https://www.ogimet.com/display_synops2.php?lang=en&lugar=78764+78762+78774+78767&tipo=ALL&ord=DIR&nil=SI&fmt=txt&ano=%s&mes=%s&day=%s&hora=%s&anof=%s&mesf=%s&dayf=%s&horaf=%s&send=send'
+
+
+log.close()
