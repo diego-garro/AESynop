ddb2fd5ab6578e1f6b3634356d7fb50c7fa85dbd
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-10-20 20:14:18 -0600

    Se completa la clase Group dentro de group.py

--- synoptic/group.py
+++ synoptic/group.py
@@ -8,6 +8,8 @@ versión: 0.0.1
 Este es un módulo para las herramientas referentes al análisis y evaluación de los reportes
 sinópticos. En específico este módulo contiene la definición de un grupo del reporte y su
 estructura. Se establecen sus características por medio de una clase.
+
+Modo: from .group import Group
 """
 
 class Group(object):
@@ -22,3 +24,16 @@ class Group(object):
     
     def more_than_five_digits(self):
         return("The group {} has more than five digits.".format(self.name))
+    
+    def verify_group_indicator(self, value, digits=1):
+        if self.group[0:digits] != value:
+            return False
+        return True
+    
+    def verify_indicator(self, indicator, range_list):
+        if indicator in range_list:
+            return True
+        return False
+    
+    def extract_indicator(self, init_index, end_index):
+        return self.group[init_index:end_index]
