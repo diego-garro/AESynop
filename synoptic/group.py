806f35a36225b910111e9b5d76a5b41e45c2228f
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-10-20 20:19:56 -0600

    Se traduce a ingles la documentacion de group.py

--- synoptic/group.py
+++ synoptic/group.py
@@ -5,16 +5,15 @@
 Package: group.py
 versión: 0.0.1
 
-Este es un módulo para las herramientas referentes al análisis y evaluación de los reportes
-sinópticos. En específico este módulo contiene la definición de un grupo del reporte y su
-estructura. Se establecen sus características por medio de una clase.
+This module contains the class that defines the structure of
+a five-digit group present within the synoptic report.
 
-Modo: from .group import Group
+Mod: from .group import Group
 """
 
 class Group(object):
     """
-    Clase que defina la base de un grupo perteneciente a cualquier reporte sinóptico.
+    Class that defines the basis of a group belonging to any synoptic report.
     """
 
     def __init__(self, group, group_name=''):
