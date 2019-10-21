db5de0a663aa26aaaffe96a63012c208e3abf4b5
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-10-20 18:58:16 -0600

    Se crea la clase Group dentro del modulo group.py

--- synoptic/group.py
+++ synoptic/group.py
@@ -9,3 +9,16 @@ Este es un módulo para las herramientas referentes al análisis y evaluación d
 sinópticos. En específico este módulo contiene la definición de un grupo del reporte y su
 estructura. Se establecen sus características por medio de una clase.
 """
+
+class Group(object):
+
+    def __init__(self, group, group_name=''):
+        self.group = group
+        self.name = group_name
+        self.len = len(group)
+    
+    def less_than_five_digits(self):
+        return("The group {} has less than five digits.".format(self.name))
+    
+    def more_than_five_digits(self):
+        return("The group {} has more than five digits.".format(self.name))
