daf24513daedbfaf67bf11fc5163dbdfae7cfd68
diegogarromolina <diego.garromolina@ucr.ac.cr>
2019-10-20 18:51:07 -0600

    Se actualiza la descripcion del modulo group.py

--- synoptic/group.py
+++ synoptic/group.py
@@ -5,5 +5,7 @@
 Package: group.py
 versión: 0.0.1
 
-Este es un módulo para las herramientas que se necesitarán en el programa principal.
+Este es un módulo para las herramientas referentes al análisis y evaluación de los reportes
+sinópticos. En específico este módulo contiene la definición de un grupo del reporte y su
+estructura. Se establecen sus características por medio de una clase.
 """
